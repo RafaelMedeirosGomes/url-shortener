f99eb038f797f2666f8110830b0f4740c17f27fe
Rafael <rafafamg@gmail.com>
2022-10-04 07:46:21 -0300

    feat: :construction: adding db connection and graceful exit for SIGINT signals

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
 import "dotenv/config";
 
+import connection from "./database/connection.mongoose";
 import app from "./app";
 
-function main(): void {
+async function main(): Promise<void> {
   const SERVER_PORT = process.env.SERVER_PORT ?? "5000";
 
-  app.listen(SERVER_PORT, () => {
+  const server = app.listen(SERVER_PORT, () => {
     console.log(`Server online on port ${SERVER_PORT}`);
+  });
+
+  process.on("SIGINT", () => {
+    server.close(() => {
+      console.log("\nStopping server");
+    });
+    connection
+      .then(async (Mongoose) => {
+        return await Mongoose.disconnect();
+      })
+      .then(() => console.log("Disconnected from db"))
+      .catch(console.error);
   });
 }
 
-main();
+void main();
