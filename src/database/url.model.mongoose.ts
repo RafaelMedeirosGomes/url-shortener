58a82e279072ded7dc8cf9f502fb72e6ac25ef8b
Rafael <rafafamg@gmail.com>
2022-10-04 09:55:07 -0300

    fix: :bug: adding indexing for shortUrl field for fast retrieves

--- src/database/url.model.mongoose.ts
+++ src/database/url.model.mongoose.ts
@@ -2,7 +2,7 @@ import { Schema, model } from "mongoose";
 
 const urlSchema = new Schema({
   longUrl: { type: String, required: true, maxlength: 2048 },
-  shortUrl: { type: String, required: true, maxlength: 22 },
+  shortUrl: { type: String, required: true, maxlength: 22, index: true },
   createdAt: { type: Date, default: Date.now },
 });
 
