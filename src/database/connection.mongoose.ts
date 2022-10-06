9f578938ad7492ee59a463852e6459193af5804b
Rafael <rafafamg@gmail.com>
2022-10-06 08:53:45 -0300

    refactor: :pencil2: fixing connection for the new env variables created

--- src/database/connection.mongoose.ts
+++ src/database/connection.mongoose.ts
@@ -1,8 +1,14 @@
 import { connect, Mongoose } from "mongoose";
 
 async function connection(): Promise<Mongoose> {
-  const hostURI = process.env.HOST_URI ?? "localhost:27017";
-  const conn = await connect(`mongodb://${hostURI}/shortener`);
+  const hostURI = process.env.DB_URI ?? "localhost:27017";
+  const username = process.env.DB_USER ?? "user";
+  const password = process.env.DB_PASS ?? "password";
+  const dbName = process.env.DB_NAME ?? "shortener";
+  const conn = await connect(hostURI, {
+    auth: { username, password },
+    dbName,
+  });
   return conn;
 }
 
