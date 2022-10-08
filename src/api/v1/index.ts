e11ad23752d7f1c8fcc52459ecce4268d6668141
Rafael <rafafamg@gmail.com>
2022-10-08 12:14:38 -0300

    refactor: :recycle: moving availableEndpoints API endpoint to the correct place

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -1,19 +1,31 @@
-import { Router } from "express";
+import { RequestHandler, Router } from "express";
 import UrlHandler from "../../handlers/url.handler";
+import { loadConfig } from "../../utils/config";
 
 export default class API {
   private readonly API_VER = "api/v1/";
-  private readonly _router: Router;
-  private readonly urlHandler: UrlHandler;
 
-  constructor(router: Router, urlHandler: UrlHandler) {
-    this._router = router;
-    this.urlHandler = urlHandler;
+  constructor(
+    private readonly _router: Router,
+    private readonly urlHandler: UrlHandler
+  ) {
     this._router.post("/create", this.urlHandler.createUrl);
-    this._router.get("/", this.urlHandler.availableEndpoints(this.API_VER));
+    this._router.get("/", this.availableEndpoints);
   }
 
   public get router(): Router {
     return this._router;
   }
+
+  private readonly availableEndpoints: RequestHandler = async (_req, res) => {
+    const { URL_PREFIX } = loadConfig();
+    res.status(200).json({
+      links: [
+        {
+          href: `${URL_PREFIX}${this.API_VER}create`,
+          type: "POST",
+        },
+      ],
+    });
+  };
 }
--- src/handlers/url.handler.ts
+++ src/handlers/url.handler.ts
@@ -1,25 +1,8 @@
 import { RequestHandler } from "express";
-import { loadConfig } from "../utils/config";
 import IShortenerService from "../services/shortener.interface";
 
 export default class UrlHandler {
   constructor(private readonly shortenerService: IShortenerService) {}
-
-  availableEndpoints(apiVersion: string): RequestHandler {
-    const handler: RequestHandler = async (_req, res) => {
-      const { URL_PREFIX } = loadConfig();
-      const prefix = URL_PREFIX;
-      res.status(200).json({
-        links: [
-          {
-            href: `${prefix}${apiVersion}create`,
-            type: "POST",
-          },
-        ],
-      });
-    };
-    return handler;
-  }
 
   createUrl: RequestHandler = async (req, res, next) => {
     const { url: longUrl } = req.body;
