8058acaf341dd9bf51db5bd7277f91ec928d59d8
Rafael <rafafamg@gmail.com>
2022-10-08 12:52:27 -0300

    fix: :bug: now if a db connection error happens it throws an Error at server start time (#14)

--- src/index.ts
+++ src/index.ts
@@ -3,23 +3,27 @@ import app from "./app";
 import { loadConfig } from "./utils/config";
 
 async function main(): Promise<void> {
-  const { SERVER_PORT } = loadConfig();
-  const server = app.listen(SERVER_PORT, () => {
-    console.log(`Server online on port ${SERVER_PORT}`);
-  });
-
-  process.on("SIGINT", async () => {
-    server.close(() => {
-      console.log("\nStopping server");
+  let conn: typeof import("mongoose");
+  try {
+    conn = await connection();
+    const { SERVER_PORT } = loadConfig();
+    const server = app.listen(SERVER_PORT, () => {
+      console.log(`Server online on port ${SERVER_PORT}`);
     });
-    const Mongoose = await connection;
-    try {
-      await Mongoose.disconnect();
-      console.log("Disconnected from db");
-    } catch (error) {
-      console.error(error);
-    }
-  });
+    process.on("SIGINT", async () => {
+      server.close(() => {
+        console.log("\nStopping server");
+      });
+      try {
+        await conn.disconnect();
+        console.log("Disconnected from db");
+      } catch (error) {
+        console.error(error);
+      }
+    });
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 void main();
